4d393ac45aa46ecc6f5c5b8414f552dcf22aa082
Fomenko Irina <irina.fomenko@mera.com>
2019-09-26 11:06:19 +0300

    Fixed class Logger

--- Logger.h
+++ Logger.h
@@ -11,22 +11,24 @@
 #include <ctime>
 
 class Logger {
+public:
     std::ofstream log_file;
-    Logger(char * fname) {
-        std::cerr << "Opening log file." << std::endl;
+    Logger(const char * fname) {
+        std::cerr << "\nOpening log file." << std::endl;
+        log_file.open(fname);
         //always append to the EOF since we need to save all our logs
-        std::ofstream log_file(fname, std::ios::app)
+        std::ofstream log_file(fname, std::ios::app);
     }
     ~Logger() {
         std::cerr << "Closing log file." << std::endl;
         log_file.close();
     }
-public:
-    void print(char * str) {
+    void print(const char * str) {
         auto current_time = std::chrono::system_clock::now();
+        time_t now = std::chrono::system_clock::to_time_t(current_time);
         if (log_file.is_open()) {
             //print time with log messages to detect when event happens
-            log_file << std::ctime(&current_time) << " | " << str << std::endl;
+            log_file << std::ctime(&now) << " | " << str << std::endl;
         } else {
             std::cerr <<"Logger doesn't work!" << std::endl;
         }
