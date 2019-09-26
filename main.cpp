f8d76295a8d18be208ab517f18b2833fcddc0b3b
Fomenko Irina <irina.fomenko@mera.com>
2019-09-26 17:07:54 +0300

    Fixed menu

--- main.cpp
+++ main.cpp
@@ -17,58 +17,26 @@ int main(int argc, char *argv[])
         }
     }
     /*---------------------------------------------*/
-    // Работа с классами
-    myQueue* obj;
-    obj = new myQueue();
-    obj->push(5);
-    obj->push(6);
-    cout << "1: ";
-    obj->Print();
-    obj->pop();
-    cout << "2: ";
-    obj->Print();
-    cout << "3: ";
-    if(obj->head_element() != 0) cout << obj->head_element() << endl;
-    else cout << "List have not elements!" << endl;
-    cout << "4: ";
-    cout << "Size: " << obj->print_size() << endl;
-
-    myDequeue* obj2 = new myDequeue;
-    obj2->push(8);
-    obj2->push_front(7);
-    obj2->push_front(9);
-    obj2->push(10);
-    obj2->push(11);
-    cout << "1: ";
-    obj2->Print();
-    obj2->pop();
-    cout << "2: ";
-    obj2->Print();
-    cout << "3: ";
-    cout << "Size: " << obj2->print_size() << endl;
-    cout << "4: ";
-    if(obj->head_element() != 0) cout << obj2->head_element() << endl;
-    else cout << "Queue have not elements!" << endl;
-    cout << "5: ";
-    obj2->pop_back();
-    obj2->Print();
-    cout << "6: ";
-    if(obj2->end_element() != 0) cout << obj2->end_element() << endl;
-    else cout << "Queue have not elements!" << endl;
-    /*---------------------------------------------*/
     Logger log("log_example.txt"); // класс Logger
-    log.print("It's work!"); // класс Logger
     /*---------------------------------------------*/
     //queue.begin = new List;//List(0);
     int el;
     int change_list = 1; //выбор списка
     int change = 1; //выбор действий
+    int change_class = 1; //выбор класса
+    int change_queue =1; //выбор действя для queue (class)
+    myQueue* myQu = new myQueue;
+    myDequeue* myDequ = new myDequeue;
     unsigned int s_time;
-    while((change_list>=1) && (change_list<=2)) {
+    while((change_list>=1) && (change_list<=3)) {
+        /*---------------------------------------------*/
+        log.print("It's work!"); // класс Logger
+        /*---------------------------------------------*/
         //выбор списка
         cout << "Change:" << endl;
         cout << "1 - Queue" << endl;
-        cout << "2 - My list" << endl;
+        cout << "2 - My queue (Structure)" << endl;
+        cout << "3 - My queue (CLass)" << endl;
         cout << "Press any key to exit" << endl;
         cin >> change_list;
 
@@ -150,6 +118,105 @@ int main(int argc, char *argv[])
                         break;
                 }
                 break;
+            case 3:
+                //выбор действия
+                cout << "Change:" << endl;
+                cout << "1 - Queue" << endl;
+                cout << "2 - Dequeue" << endl;
+                cout << "3 - Quit" << endl;
+                cin >> change_class;
+
+                switch (change_class){
+                    case 1:
+                        cout << "Change:" << endl;
+                        cout << "1 - Add element to back" << endl;
+                        cout << "2 - Delete first element" << endl;
+                        cout << "3 - Print first element" << endl;
+                        cout << "4 - Print size" << endl;
+                        cout << "5 - Print list" << endl;
+                        cout << "6 - Quit" << endl;
+                        cin >> change_queue;
+
+                        switch (change_queue){
+                            case 1:
+                                cout << "Enter element: ";
+                                cin >> el;
+                                myQu->push(el);
+                                break;
+                            case 2:
+                                myQu->pop();
+                                break;
+                            case 3:
+                                if(myQu->head_element() != 0) cout << myQu->head_element() << endl;
+                                else cout << "Queue have not elements!" << endl;
+                                break;
+                            case 4:
+                                cout << "Size: " << myQu->print_size() << endl;
+                                break;
+                            case 5:
+                                myQu->Print();
+                                break;
+                            case 6:
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 2:
+                        //выбор действия
+                        cout << "Change:" << endl;
+                        cout << "1 - Add element to back" << endl;
+                        cout << "2 - Add element to front" << endl;
+                        cout << "3 - Delete first element" << endl;
+                        cout << "4 - Delete last element" << endl;
+                        cout << "5 - Print first element" << endl;
+                        cout << "6 - Print last element" << endl;
+                        cout << "7 - Print size" << endl;
+                        cout << "8 - Print list" << endl;
+                        cout << "9 - Quit" << endl;
+                        cin >> change;
+
+                        switch (change) {
+                            case 1:
+                                cout << "Enter element: ";
+                                cin >> el;
+                                myDequ->push(el);
+                                break;
+                            case 2:
+                                cout << "Enter element: ";
+                                cin >> el;
+                                myDequ->push_front(el);
+                                break;
+                            case 3:
+                                myDequ->pop();
+                                break;
+                            case 4:
+                                myDequ->pop_back();
+                                break;
+                            case 5:
+                                if(myDequ->head_element() != 0) cout << myDequ->head_element() << endl;
+                                else cout << "Queue have not elements!" << endl;
+                                break;
+                            case 6:
+                                if(myDequ->end_element() != 0) cout << myDequ->end_element() << endl;
+                                else cout << "Queue have not elements!" << endl;
+                                break;
+                            case 7:
+                                cout << "Size: " << myDequ->print_size() << endl;
+                                break;
+                            case 8:
+                                myDequ->Print();
+                                break;
+                            case 9:
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
             default:
                 break;
         }
