b3bd1836c209e6c464ec7384c6d548661c345bc9
Fomenko Irina <irina.fomenko@mera.com>
2019-09-26 14:14:35 +0300

    Work with class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "newqueue.h"
 #include "myqueue.h"
 #include "Logger.h"
+#include "Structure.h"
 
 using namespace std;
 
@@ -15,8 +16,27 @@ int main(int argc, char *argv[])
             push(el);
         }
     }
-    Logger log("log_example.txt");
-    log.print("It's work!");
+    /*---------------------------------------------*/
+    // Работа с классами
+    myQueue* obj;
+    obj = new myQueue();
+    cout << "qw" << endl;
+    obj->push(5);
+    obj->push(6);
+    cout << "1: ";
+    obj->Print();
+    obj->pop();
+    cout << "2: ";
+    obj->Print();
+    cout << "3: ";
+    if(obj->head_element() != 0) cout << obj->head_element() << endl;
+    else cout << "List have not elements!" << endl;
+    cout << "4: ";
+    cout << "Size: " << obj->print_size() << endl;
+    /*---------------------------------------------*/
+    Logger log("log_example.txt"); // класс Logger
+    log.print("It's work!"); // класс Logger
+    /*---------------------------------------------*/
     //queue.begin = new List;//List(0);
     int el;
     int change_list = 1; //выбор списка
