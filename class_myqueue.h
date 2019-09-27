6d3821033cd280e552cfdcbfa63b41245d0a604b
Fomenko Irina <irina.fomenko@mera.com>
2019-09-27 16:37:03 +0300

    Moved class to a new file class_myqueue

--- class_myqueue.h
+++ class_myqueue.h
@@ -5,4 +5,95 @@
 #ifndef SPISOK_2_CLASS_MYQUEUE_H
 #define SPISOK_2_CLASS_MYQUEUE_H
 
+#include "Structure.h"
+
+class myQueue: public Structure{
+public:
+    myQueue() {};
+    ~myQueue()
+    {
+        while(begin)
+        {
+            List* tmp = begin;
+            begin = begin->next;
+            delete tmp;
+        }
+    }
+
+    void push(int n)
+    {
+        if(end != NULL)
+        {
+            List* temp = new List;
+            temp->num = n;
+            temp->next = NULL;
+            end->next = temp;
+            end = end->next;
+        }
+        else
+        {
+            end = new List;
+            end->num = n;
+            end->next = NULL;
+            begin = end;
+        }
+        size++;
+    }
+
+    void pop()
+    {
+        try{
+            if(begin == NULL) {throw ex2;}
+            if (begin == end) end = NULL;
+            List* el = begin;
+            begin = begin->next;
+            delete el;
+            size--;
+        }
+        catch(std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    int head_element()
+    {
+        if(begin != NULL)
+        {
+            int el = begin->num;
+            return el;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int print_size()
+    {
+        return size;
+    }
+
+    void Print()
+    {
+        try{
+            List* temp = begin;
+            if(temp == NULL)
+            {
+                throw ex2;
+            }
+            while (temp != NULL)
+            {
+                int el = temp->num;
+                std::cout << el << " ";
+                temp = temp->next;
+            }
+            std::cout << std::endl;
+        }
+        catch (std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+
+    }
+};
+
 #endif //SPISOK_2_CLASS_MYQUEUE_H
