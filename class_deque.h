a6692e6ef2c159bc0529226bf6c8afce32e96dce
Fomenko Irina <irina.fomenko@mera.com>
2019-09-27 16:36:36 +0300

    Moved class to a new file class_deque

---
+++ class_deque.h
@@ -0,0 +1,101 @@
+//
+// Created by ifomenko on 27.09.2019.
+//
+
+#ifndef SPISOK_2_CLASS_DEQUE_H
+#define SPISOK_2_CLASS_DEQUE_H
+
+#include "class_myqueue.h"
+
+class myDeque: public myQueue {
+public:
+    myDeque(){};
+    ~myDeque()
+    {
+        while(begin)
+        {
+            List* tmp = begin;
+            begin = begin->next;
+            delete tmp;
+        }
+    };
+
+    void push(int n)
+    {
+        if(end != NULL)
+        {
+            List* temp = new List;
+            temp->num = n;
+            temp->next = NULL;
+            temp->prev = end;
+            end->next = temp;
+            end = end->next;
+        }
+        else
+        {
+            end = new List;
+            end->num = n;
+            end->next = NULL;
+            end->prev = NULL;
+            begin = end;
+        }
+        size++;
+    }
+
+    void push_front(int n)
+    {
+        if(begin != NULL)
+        {
+            List* tmp = new List;
+            tmp->num = n;
+            tmp->prev = NULL;
+            tmp->next = begin;
+            begin->prev = tmp;
+            begin = tmp;
+        }
+        else
+        {
+            begin = new List;
+            begin->num = n;
+            begin->prev = NULL;
+            begin->next = NULL;
+            end = begin;
+        }
+        size++;
+    }
+
+    void pop_back()
+    {
+        try{
+            if(end == NULL) {throw ex2;}
+            if (begin == end) begin = NULL;
+            List* el = end;
+            end = end->prev;
+            delete el;
+            if(size != 1)
+            {
+                end->next = NULL;
+            }
+            size--;
+        }
+        catch(std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+
+    }
+
+    int end_element()
+    {
+        if(end!=NULL)
+        {
+            int el = end->num;
+            return el;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
+
+#endif //SPISOK_2_CLASS_DEQUE_H
---
+++ class_myqueue.h
@@ -0,0 +1,8 @@
+//
+// Created by ifomenko on 27.09.2019.
+//
+
+#ifndef SPISOK_2_CLASS_MYQUEUE_H
+#define SPISOK_2_CLASS_MYQUEUE_H
+
+#endif //SPISOK_2_CLASS_MYQUEUE_H
