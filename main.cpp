9924c0759990913ae98823999d2a49d05d5b104d
Fomenko Irina <irina.fomenko@mera.com>
2019-10-01 15:51:42 +0300

    Fixed menu

--- main.cpp
+++ main.cpp
@@ -25,8 +25,7 @@ int main(int argc, char *argv[])
     int change = 1; //выбор действий
     int change_class = 1; //выбор класса
     int change_queue =1; //выбор действя для queue (class)
-    unsigned int s_time;
-    while((change_class>=1) && (change_class<=2))
+    while((change_class >= 1) && (change_class <= 2))
     {
         /*---------------------------------------------*/
         log.print("It's work!"); // класс Logger
@@ -38,119 +37,105 @@ int main(int argc, char *argv[])
         cout << "Press any key to exit" << endl;
         cin >> change_class;
 
-        switch (change_class)
+        try
         {
-            case 1:
-                cout << "Change:" << endl;
-                cout << "1 - Add element to back" << endl;
-                cout << "2 - Delete first element" << endl;
-                cout << "3 - Print first element" << endl;
-                cout << "4 - Print size" << endl;
-                cout << "5 - Print queue" << endl;
-                cout << "6 - Quit" << endl;
-                cin >> change_queue;
+            switch (change_class)
+            {
+                case 1:
+                    cout << "Change:" << endl;
+                    cout << "1 - Add element to back" << endl;
+                    cout << "2 - Delete first element" << endl;
+                    cout << "3 - Print first element" << endl;
+                    cout << "4 - Print size" << endl;
+                    cout << "5 - Print queue" << endl;
+                    cout << "6 - Quit" << endl;
+                    cin >> change_queue;
 
-                switch (change_queue)
-                {
-                    case 1:
-                        cout << "Enter element: ";
-                        cin >> el;
-                        myQu->push(el);
-                        break;
-                    case 2:
-                        myQu->pop();
-                        break;
-                    case 3:
-                        try
-                        {
+                    switch (change_queue)
+                    {
+                        case 1:
+                            cout << "Enter element: ";
+                            cin >> el;
+                            myQu->push(el);
+                            break;
+                        case 2:
+                            myQu->pop();
+                            break;
+                        case 3:
                             if(myQu->head_element() == 0) {throw ex2;}
                             cout << myQu->head_element() << endl;
-                        }
-                        catch (std::exception& e)
-                        {
-                            std::cout << e.what() << std::endl;
-                        }
-                        break;
-                    case 4:
-                        cout << "Size: " << myQu->print_size() << endl;
-                        break;
-                    case 5:
-                        myQu->Print();
-                        break;
-                    case 6:
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            case 2:
-                //выбор действия
-                cout << "Change:" << endl;
-                cout << "1 - Add element to back" << endl;
-                cout << "2 - Add element to front" << endl;
-                cout << "3 - Delete first element" << endl;
-                cout << "4 - Delete last element" << endl;
-                cout << "5 - Print first element" << endl;
-                cout << "6 - Print last element" << endl;
-                cout << "7 - Print size" << endl;
-                cout << "8 - Print deque" << endl;
-                cout << "9 - Quit" << endl;
-                cin >> change;
+                            break;
+                        case 4:
+                            cout << "Size: " << myQu->print_size() << endl;
+                            break;
+                        case 5:
+                            myQu->Print();
+                            break;
+                        case 6:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 2:
+                    //выбор действия
+                    cout << "Change:" << endl;
+                    cout << "1 - Add element to back" << endl;
+                    cout << "2 - Add element to front" << endl;
+                    cout << "3 - Delete first element" << endl;
+                    cout << "4 - Delete last element" << endl;
+                    cout << "5 - Print first element" << endl;
+                    cout << "6 - Print last element" << endl;
+                    cout << "7 - Print size" << endl;
+                    cout << "8 - Print deque" << endl;
+                    cout << "9 - Quit" << endl;
+                    cin >> change;
 
-                switch (change)
-                {
-                    case 1:
-                        cout << "Enter element: ";
-                        cin >> el;
-                        myDequ->push(el);
-                        break;
-                    case 2:
-                        cout << "Enter element: ";
-                        cin >> el;
-                        myDequ->push_front(el);
-                        break;
-                    case 3:
-                        myDequ->pop();
-                        break;
-                    case 4:
-                        myDequ->pop_back();
-                        break;
-                    case 5:
-                        try
-                        {
+                    switch (change)
+                    {
+                        case 1:
+                            cout << "Enter element: ";
+                            cin >> el;
+                            myDequ->push(el);
+                            break;
+                        case 2:
+                            cout << "Enter element: ";
+                            cin >> el;
+                            myDequ->push_front(el);
+                            break;
+                        case 3:
+                            myDequ->pop();
+                            break;
+                        case 4:
+                            myDequ->pop_back();
+                            break;
+                        case 5:
                             if(myDequ->head_element() == 0) {throw ex2;}
                             cout << myDequ->head_element() << endl;
-                        }
-                        catch (std::exception& e)
-                        {
-                            std::cout << e.what() << std::endl;
-                        }
-                        break;
-                    case 6:
-                        try
-                        {
+                            break;
+                        case 6:
                             if(myDequ->end_element() == 0) {throw ex2;}
                             cout << myDequ->end_element() << endl;
-                        }
-                        catch (std::exception& e)
-                        {
-                            std::cout << e.what() << std::endl;
-                        }
-                        break;
-                    case 7:
-                        cout << "Size: " << myDequ->print_size() << endl;
-                        break;
-                    case 8:
-                        myDequ->Print();
-                        break;
-                    case 9:
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            default:
-                break;
+                            break;
+                        case 7:
+                            cout << "Size: " << myDequ->print_size() << endl;
+                            break;
+                        case 8:
+                            myDequ->Print();
+                            break;
+                        case 9:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        catch (std::exception& e)
+        {
+            std::cout << e.what() << std::endl;
         }
 
     }
