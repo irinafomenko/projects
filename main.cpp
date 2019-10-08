22b0602249f2148684e845c37cc8a2a31708f554
Fomenko Irina <irina.fomenko@mera.com>
2019-10-08 12:19:40 +0300

    fixed threads, added logger

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include "Logger.h"
 #include "class_deque.h"
-#include <windows.h>
 #include <functional>
 #include <thread>
 #include <mutex>
 #include <list>
-#include <map>
-#include <queue>
 //#include "queue_menu.h"
 
 using namespace std;
@@ -20,6 +17,9 @@ list <string> my_commands;
 list <int> elements;
 mutex mut;
 /*---------------------------------------------*/
+/*---------------------------------------------*/
+Logger log("log_example.txt"); // класс Logger
+/*---------------------------------------------*/
 
 int xtime = 10;
 int change = 1;
@@ -27,7 +27,8 @@ void func_for_thread()
 {
     cout << "thread" << endl;
     int n = 0;
-    while(change != 9) {
+    while(change != 9)
+    {
         this_thread::sleep_for(std::chrono::seconds(xtime));
         list<int>::iterator el = elements.begin();
         for (list<string>::iterator k = my_commands.begin(); k != my_commands.end(); ++k) {
@@ -45,6 +46,9 @@ void func_for_thread()
             if (*k == "end_element") { cout << myDequ->end_element() << endl; }
             if (*k == "size_of_queue") { cout << myDequ->size_of_queue() << endl; }
             if (*k == "print") { myDequ->print(); }
+            /*---------------------------------------------*/
+            log.print("It's thread 2! | Выполнил команду!"); // класс Logger
+            /*---------------------------------------------*/
         }
         //break;
         //n++;
@@ -53,26 +57,16 @@ void func_for_thread()
     }
 }
 
-int main(int argc, char *argv[])
+void main_menu()
 {
-
-    if(argc>1)
-    {
-        for(int i = 1; i < argc; i++)
-        {
-            int el = atoi(argv[i]);
-            myQu->push(el);
-            myDequ->push(el);
-        }
-    }
     /*---------------------------------------------*/
     thread my_thread_2(func_for_thread); //создание потоков
-
-    //unique_lock<std::mutex> guard(m, std::defer_lock);
     /*---------------------------------------------*/
 
+
     int el;
-    while((change >= 1) && (change <= 8)) {
+    while((change >= 1) && (change <= 8))
+    {
         cout << "1 - Add element to back" << endl;
         cout << "2 - Add element to front" << endl;
         cout << "3 - Delete first element" << endl;
@@ -150,9 +144,30 @@ int main(int argc, char *argv[])
         {
             std::cout << e.what() << std::endl;
         }
-
+        /*---------------------------------------------*/
+        log.print("It's thread 1! | Получил команду!"); // класс Logger
+        /*---------------------------------------------*/
     }
     my_thread_2.join();
+
+}
+
+int main(int argc, char *argv[])
+{
+
+    if(argc>1)
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            int el = atoi(argv[i]);
+            myQu->push(el);
+            myDequ->push(el);
+        }
+    }
+    /*---------------------------------------------*/
+    thread my_thread_1(main_menu); //главное меню
+    my_thread_1.join();
+    /*---------------------------------------------*/
 
     return 0;
 }
