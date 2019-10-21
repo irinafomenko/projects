c18e8b0e1226d89fe6a46eb19639bc7df7378d30
Fomenko Irina <irina.fomenko@mera.com>
2019-10-21 16:18:36 +0300

    Fixed Logger and Added functions

--- class_deque.h
+++ class_deque.h
@@ -7,6 +7,8 @@
 
 #include "class_myqueue.h"
 
+Logger logg_deque("log_example.txt", "Deque");
+
 class myDeque: public myQueue {
 public:
     myDeque(){};
@@ -22,45 +24,45 @@ public:
 
     void push(int n)
     {
-        logg.print("push() function of deque");
+        logg_deque.print("push() function of deque");
         if(end != NULL)
         {
             List* temp = new List(n, NULL, end);
             end->next = temp;
             end = end->next;
-            logg.print("Add element " + std::to_string(n));
+            logg_deque.print("Add element " + std::to_string(n));
         }
         else
         {
             end = new List(n, NULL, NULL);
             begin = end;
-            logg.print("Add element " + std::to_string(n));
+            logg_deque.print("Add element " + std::to_string(n));
         }
         size++;
     }
 
     void push_front(int n)
     {
-        logg.print("push_front() function of deque");
+        logg_deque.print("push_front() function of deque");
         if(begin != NULL)
         {
             List* tmp = new List(n, begin, NULL);
             begin->prev = tmp;
             begin = tmp;
-            logg.print("Add element " + std::to_string(n));
+            logg_deque.print("Add element " + std::to_string(n));
         }
         else
         {
             begin = new List(n, NULL, NULL);
             end = begin;
-            logg.print("Add element " + std::to_string(n));
+            logg_deque.print("Add element " + std::to_string(n));
         }
         size++;
     }
 
     void pop_back()
     {
-        logg.print("pop_back() function of deque");
+        logg_deque.print("pop_back() function of deque");
         if(end == NULL) {throw ex2;}
         if (begin == end) {begin = NULL;}
         List* el = end;
@@ -71,15 +73,15 @@ public:
             end->next = NULL;
         }
         size--;
-        logg.print("Deleted last element");
+        logg_deque.print("Deleted last element");
     }
 
     int begin_element()
     {
-        logg.print("begin_element() function of deque");
+        logg_deque.print("begin_element() function of deque");
         if(begin != NULL)
         {
-            logg.print("Return begin element");
+            logg_deque.print("Return begin element");
             return begin->num;
         }
         throw ex2;
@@ -88,14 +90,50 @@ public:
 
     int end_element()
     {
-        logg.print("end_element() function of deque");
+        logg_deque.print("end_element() function of deque");
         if(end != NULL)
         {
-            logg.print("Return end element");
+            logg_deque.print("Return end element");
             return end->num;
         }
         throw ex2;
         //return 0;
+    }
+
+    void pop()
+    {
+        logg_deque.print("pop() function");
+        if(begin == NULL) {throw ex2;}
+        if(begin == end) {end = NULL;}
+        List* el = begin;
+        begin = begin->next;
+        delete el;
+        size--;
+        logg_deque.print("Deleted first element");
+    }
+
+    int size_of_queue()
+    {
+        logg_deque.print("size_of_queue() function");
+        logg_deque.print("Return size of queue");
+        return size;
+    }
+
+    void print()
+    {
+        logg_deque.print("print() function");
+        if(begin == NULL) {throw ex2;}
+        List* temp = begin;
+        std::string queue;
+        while (temp != NULL)
+        {
+            int el = temp->num;
+            std::cout << el << " ";
+            queue += " " + std::to_string(el);
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+        logg_deque.print("Print queue" + queue);
     }
 };
 
