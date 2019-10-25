dc698e80a908c8ad46b275165e153f95823fe8b4
Fomenko Irina <irina.fomenko@mera.com>
2019-10-25 15:19:20 +0300

    Fixed main function

--- main.cpp
+++ main.cpp
@@ -1,159 +1,24 @@
 #include <iostream>
 #include "Logger.h"
 #include "class_deque.h"
-#include <functional>
-#include <thread>
-#include <mutex>
-#include <list>
-//#include "queue_menu.h"
+#include "server.h"
+#include "client.h"
 
 using namespace std;
 
-/*---------------------------------------------*/
-myDeque* myDequ = new myDeque;
-myQueue* my_command = new myQueue;
-mutex mut;
-/*---------------------------------------------*/
+#define CLIENT "client"
+
 /*---------------------------------------------*/
 Logger log("log_example.txt"); // класс Logger
 /*---------------------------------------------*/
 
-int xtime = 1;
-int change = 1;
-void func_for_thread()
+int main(int argc, char *argv[])
 {
-    /*---------------------------------------------*/
-    log.print("It's thread 2! | Enter!"); // класс Logger
-    /*---------------------------------------------*/
-    //cout << "thread" << endl;
-    while(change != 9)
+    if(argc>1)
     {
-        this_thread::sleep_for(std::chrono::seconds(xtime));
-
-        mut.lock();
-        //for(int i = 0; i < my_command->size_of_queue(); i++)
-        while(my_command->size_of_queue() != 0)
-        {
-            try
-            {
-                pair<std::string, int> k = my_command->head();
-                cout << k.first << endl;// чтобы знать какая команда выполняется
-                if (k.first == "push") { myDequ->push(k.second); }
-                if (k.first == "push_front") { myDequ->push_front(k.second); }
-                if (k.first == "pop") { myDequ->pop(); }
-                if (k.first == "pop_back") { myDequ->pop_back(); }
-                if (k.first == "begin_element") { cout << myDequ->begin_element() << endl; }
-                if (k.first == "end_element") { cout << myDequ->end_element() << endl; }
-                if (k.first == "size_of_queue") { cout << myDequ->size_of_queue() << endl; }
-                if (k.first == "print") { myDequ->print();}
-            }
-            catch (std::exception& e)
-            {
-                std::cout << e.what() << std::endl;
-            }
-            try
-            {
-                my_command->pop();// чтобы очередь команд каждый раз не повторялась
-            }
-            catch (std::exception& e)
-            {
-                std::cout << e.what() << std::endl;
-            }
-        }
-        mut.unlock();
-        //my_command->pop();
-        /*---------------------------------------------*/
-        log.print("It's thread 2! | Exit!"); // класс Logger
-        /*---------------------------------------------*/
+        if(! strcmp(argv[1],CLIENT)) {main_client();}
+        else {main_server();}
     }
-}
-
-void main_menu()
-{
-    /*---------------------------------------------*/
-    thread my_thread_2(func_for_thread); //создание потоков
-    /*---------------------------------------------*/
-    /*---------------------------------------------*/
-    log.print("It's thread 1! | Enter!"); // класс Logger
-    /*---------------------------------------------*/
-
-    int el;
-    while((change >= 1) && (change <= 8))
-    {
-        //mut.lock();//чтобы второй поток ничего не выводил пока не выбрана команда
-        cout << "1 - Add element to back" << endl;
-        cout << "2 - Add element to front" << endl;
-        cout << "3 - Delete first element" << endl;
-        cout << "4 - Delete last element" << endl;
-        cout << "5 - Print first element" << endl;
-        cout << "6 - Print last element" << endl;
-        cout << "7 - Print size" << endl;
-        cout << "8 - Print deque" << endl;
-        cout << "9 - Quit" << endl;
-        //mut.lock();
-        cin >> change;
-        //mut.unlock();
-
-        //try
-        //{
-            switch (change) {
-                case 1:
-                    mut.lock();//также чтобы не разрывались строчки
-                    cout << "Enter element: ";
-                    cin >> el;
-                    mut.unlock();
-                    my_command->push("push", el);
-                    break;
-                case 2:
-                    mut.lock();
-                    cout << "Enter element: ";
-                    cin >> el;
-                    mut.unlock();
-                    my_command->push("push_front", el);
-                    break;
-                case 3:
-                    my_command->push( "pop");
-                    break;
-                case 4:
-                    my_command->push( "pop_back");
-                    break;
-                case 5:
-                    my_command->push( "begin_element");
-                    break;
-                case 6:
-                    my_command->push( "end_element");
-                    break;
-                case 7:
-                    my_command->push( "size_of_queue");
-                    break;
-                case 8:
-                    my_command->push( "print");
-                    break;
-                case 9:
-                    break;
-                default:
-                    break;
-            }
-        //}
-        //catch (std::exception& e)
-        //{
-        //    std::cout << e.what() << std::endl;
-        //}
-        /*---------------------------------------------*/
-        log.print("It's thread 1! | Exit!"); // класс Logger
-        /*---------------------------------------------*/
-    }
-    my_thread_2.join();
-
-}
-
-int main()
-{
-
-    /*---------------------------------------------*/
-    thread my_thread_1(main_menu); //главное меню
-    my_thread_1.join();
-    /*---------------------------------------------*/
 
     return 0;
 }
