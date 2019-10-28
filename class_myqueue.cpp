f5e1a4db512a92101704fbd77e056034df34522a
Fomenko Irina <irina.fomenko@mera.com>
2019-10-28 16:25:58 +0300

    Fixed class myqueue

--- class_myqueue.cpp
+++ class_myqueue.cpp
@@ -1,4 +1,96 @@
 //
 // Created by ifomenko on 28.10.2019.
 //
+#include "class_myqueue.h"
 
+myexception ex2;
+Logger logg_queue("log_example.txt", "Queue");
+
+    /*
+    void push(int n)
+    {
+        logg.print("push() function");
+        if(end != NULL)
+        {
+            List* temp = new List(n, NULL);
+            end->next = temp;
+            end = end->next;
+            logg.print("Add element");
+        }
+        else
+        {
+            end = new List(n, NULL);
+            begin = end;
+            logg.print("Add element");
+        }
+        size++;
+    }*/
+
+void myQueue::push(std::string com, int n)
+{
+    logg_queue.print("push function of queue");
+    if(end != NULL)
+    {
+        List* temp = new List(n, NULL, com);
+        end->next = temp;
+        end = end->next;
+        if(n != 0) {logg_queue.print("Add element " + std::to_string(n));}
+        else {logg_queue.print("Add command " + com);}
+    }
+    else
+    {
+        end = new List(n, NULL, com);
+        begin = end;
+        if(n != 0) {logg_queue.print("Add element " + std::to_string(n));}
+        else {logg_queue.print("Add command " + com);}
+    }
+    size++;
+}
+
+std::pair<std::string, int> myQueue::head()
+{
+    logg_queue.print("head() function of queue");
+    if(begin != NULL)
+    {
+        logg_queue.print("Return head element");
+        return begin->commands;
+    }
+    throw ex2;
+    //return 0;
+}
+
+void myQueue::pop()
+{
+    logg_queue.print("pop() function");
+    if(begin == NULL) {throw ex2;}
+    if(begin == end) {end = NULL;}
+    List* el = begin;
+    begin = begin->next;
+    delete el;
+    size--;
+    logg_queue.print("Deleted first element");
+}
+
+int myQueue::size_of_queue()
+{
+    logg_queue.print("size_of_queue() function");
+    logg_queue.print("Return size of queue");
+    return size;
+}
+
+void myQueue::print()
+{
+    logg_queue.print("print() function");
+    if(begin == NULL) {throw ex2;}
+    List* temp = begin;
+    std::string queue;
+    while (temp != NULL)
+    {
+        int el = temp->num;
+        std::cout << el << " ";
+        queue += " " + std::to_string(el);
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+    logg_queue.print("Print queue" + queue);
+}
