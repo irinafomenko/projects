90c3d6c4f5569d42ace5c500ad0197af833ada92
Fomenko Irina <irina.fomenko@mera.com>
2019-11-07 16:56:55 +0300

    Added have_elements_in_response() and msg logger

--- server.cpp
+++ server.cpp
@@ -15,15 +15,20 @@ using namespace std;
 Logger log_server("log_example.txt", "Server"); // класс Logger
 /*---------------------------------------------*/
 
+void have_elements_in_response(int sock)
+{
+    char have_elements[4] = "yes";
+    int size_have_elements = sizeof(have_elements);
+    send(sock, &size_have_elements, sizeof(int), 0);
+    send(sock, &have_elements, size_have_elements, 0);
+}
+
 void main_server(const char *ip_addr)
 {
     /*---------------------------------------------*/
     log_server.print("main_server()"); // класс Logger
     /*---------------------------------------------*/
     int size_queue;
-    int bytes_size_queue;
-    char have_elements[4] = "yes";
-    int size_have_elements = sizeof(have_elements);
     myDeque* myDequ = new myDeque;
     myQueue* my_command = new myQueue;
     int sock, listener;//для сервера
@@ -32,6 +37,7 @@ void main_server(const char *ip_addr)
     if(listener < 0)
     {
         perror("socket");
+        log_server.print("error socket"); // класс Logger
         exit(1);
     }
 
@@ -41,6 +47,7 @@ void main_server(const char *ip_addr)
     if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)//связывание сокета с адресом
     {
         perror("bind");
+        log_server.print("error bind"); // класс Logger
         exit(2);
     }
 
@@ -50,38 +57,38 @@ void main_server(const char *ip_addr)
     {
         /*--------------------------------------*/
         //вывод сообщения о подключении клиента и отправка ответного сообщения
-        if((sock = accept(listener, NULL, NULL)) >= 0);//создает для общения с клиентом новый сокет
-        {
-            //получение ip адреса клиента
-            int size_ip_addr_client;
-            recv(sock, &size_ip_addr_client, sizeof(int), 0);
-            char ip_addr_client[size_ip_addr_client];
-            recv(sock, &ip_addr_client, size_ip_addr_client, 0);
-            string msg_for_logger = "New client connected! ";
-            /*--------------------------------------*/
-            log_server.print(msg_for_logger + ip_addr_client); // класс Logger
-            /*--------------------------------------*/
-            cout << "New client connected! " << ip_addr_client << endl;
-            //отправка сообщения клиенту
-            int size_msg = 18;
-            char msg[size_msg];
-            strcpy(msg,"Hello from server!");
-            send(sock, &size_msg, sizeof(int), 0);
-            send(sock, &msg, size_msg, 0);
-        }
+        //создает для общения с клиентом новый сокет
+        if((sock = accept(listener, NULL, NULL)) < 0) {break;}
+        //получение ip адреса клиента
+        int size_ip_addr_client;
+        recv(sock, &size_ip_addr_client, sizeof(int), 0);
+        char ip_addr_client[size_ip_addr_client];
+        recv(sock, &ip_addr_client, size_ip_addr_client, 0);
+        string msg_for_logger = "New client connected! ";
         /*--------------------------------------*/
-        bytes_size_queue = recv(sock, &size_queue, sizeof(int), 0);
-        if(bytes_size_queue <= 0) {break;}
+        log_server.print(msg_for_logger + ip_addr_client); // класс Logger
+        /*--------------------------------------*/
+        cout << "New client connected! " << ip_addr_client << endl;
+        //отправка сообщения клиенту
+        int size_msg = 18;
+        char msg[size_msg];
+        strcpy(msg,"Hello from server!");
+        send(sock, &size_msg, sizeof(int), 0);
+        send(sock, &msg, size_msg, 0);
+
+        /*--------------------------------------*/
+        recv(sock, &size_queue, sizeof(int), 0);
 
         for(int i=0; i<size_queue; i++)
         {
-            int el_second;
             int size_cmd;
+            pair<string, int> el;
             recv(sock, &size_cmd, sizeof(int), 0);
             char el_first[size_cmd];
             recv(sock, &el_first, size_cmd, 0);
-            recv(sock, &el_second, sizeof(int), 0);
-            my_command->push(el_first, el_second);
+            recv(sock, &el.second, sizeof(int), 0);
+            el.first = el_first;
+            my_command->push(el);
         }
 
         while(my_command->size_of_queue() != 0)
@@ -95,44 +102,38 @@ void main_server(const char *ip_addr)
                 if (k.first == "pop")
                 {
                     myDequ->pop();
-                    send(sock, &size_have_elements, sizeof(int), 0);
-                    send(sock, &have_elements, size_have_elements, 0);
+                    have_elements_in_response(sock);
                 }
                 if (k.first == "pop_back")
                 {
                     myDequ->pop_back();
-                    send(sock, &size_have_elements, sizeof(int), 0);
-                    send(sock, &have_elements, size_have_elements, 0);
+                    have_elements_in_response(sock);
                 }
                 if (k.first == "begin_element")
                 {
                     //cout << myDequ->begin_element() << endl;
                     int begin_el = myDequ->begin_element();
-                    send(sock, &size_have_elements, sizeof(int), 0);
-                    send(sock, &have_elements, size_have_elements, 0);
+                    have_elements_in_response(sock);
                     send(sock, &begin_el, sizeof(int), 0);
                 }
                 if (k.first == "end_element")
                 {
                     //cout << myDequ->end_element() << endl;
                     int end_el = myDequ->end_element();
-                    send(sock, &size_have_elements, sizeof(int), 0);
-                    send(sock, &have_elements, size_have_elements, 0);
+                    have_elements_in_response(sock);
                     send(sock, &end_el, sizeof(int), 0);
                 }
                 if (k.first == "size_of_queue")
                 {
                     //cout << myDequ->size_of_queue() << endl;
                     int size_of_queue = myDequ->size_of_queue();
-                    send(sock, &size_have_elements, sizeof(int), 0);
-                    send(sock, &have_elements, size_have_elements, 0);
+                    have_elements_in_response(sock);
                     send(sock, &size_of_queue, sizeof(int), 0);
                 }
                 if (k.first == "print")
                 {
                     myDequ->print();
-                    send(sock, &size_have_elements, sizeof(int), 0);
-                    send(sock, &have_elements, size_have_elements, 0);
+                    have_elements_in_response(sock);
                     int size = myDequ->size_of_queue();
                     send(sock, &size, sizeof(int), 0);
                     myDeque* send_deque = new myDeque;
@@ -149,8 +150,9 @@ void main_server(const char *ip_addr)
             {
                 //std::cout << e.what() << std::endl;
                 string msg_str = e.what();
+                //const char *msg_str = e.what();
                 int size_msg = msg_str.length();
-                char msg[msg_str.length()];
+                char msg[size_msg];
                 strcpy(msg,msg_str.c_str());
                 send(sock, &size_msg, sizeof(int), 0);
                 send(sock, &msg, size_msg, 0);
--- server.h
+++ server.h
@@ -1,10 +1,11 @@
 //
 // Created by ifomenko on 28.10.2019.
 //
-
+#include "server.cpp"
 #ifndef SPISOK_2_SERVER_H
 #define SPISOK_2_SERVER_H
 
+void have_elements_in_response(int sock);
 void main_server(const char *ip_addr);
 
 #endif //SPISOK_2_SERVER_H
