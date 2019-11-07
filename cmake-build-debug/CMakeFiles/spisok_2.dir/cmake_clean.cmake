5102018dfc79ec87404755945f008c6334dbdd98
Fomenko Irina <irina.fomenko@mera.com>
2019-11-07 16:54:18 +0300

    Deleded clienr.cpp and server.cpp

---
+++ cmake-build-debug/CMakeFiles/spisok_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spisok_2.dir/main.cpp.o"
+  "CMakeFiles/spisok_2.dir/class_myqueue.cpp.o"
+  "CMakeFiles/spisok_2.dir/class_deque.cpp.o"
+  "spisok_2.pdb"
+  "spisok_2.exe"
+  "libspisok_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spisok_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
