85a55fc18962e11209dac91f37449c4e24150053
Fomenko Irina <irina.fomenko@mera.com>
2019-11-15 17:07:35 +0300

    Added functions

--- client.h
+++ client.h
@@ -6,7 +6,10 @@
 #define SPISOK_2_CLIENT_H
 
 void main_client(const char *ip_addr);
-void thread_for_send_to_server(const char *ip_addr);
-void send_to_server(const char *ip_addr);
+void menu();
+void thread_for_dialog_with_server(const char *ip_addr);
+void query_execution();
+void send_to_server();
+void messages_with_server(const char *ip_addr);
 
 #endif //SPISOK_2_CLIENT_H
